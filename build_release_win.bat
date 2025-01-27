bc60fff567c69f9cf070a5a1d064d512089ec6f8
scx1332 <sieciech.czajka@golem.network>
2025-01-27 13:25:15 +0100

    Wip - commands

---
+++ build_release_win.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\amd64\MSBuild.exe" cmake_and_cuda.sln /p:Platform=x64 /p:Configuration=Release /t:profanity_cuda
--- test.cu
+++ test.cu
@@ -24,6 +24,8 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#include "help.hpp"
+#include "ArgParser.hpp"
 #include "particle.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -801,6 +803,66 @@ static void printResult(const uint64_t seed[4], uint64_t round, result r, uint8_
 
 int main(int argc, char ** argv)
 {
+
+		ArgParser argp(argc, argv);
+		bool bHelp = false;
+		bool bModeBenchmark = false;
+		bool bModeZeros = false;
+		bool bModeZeroBytes = false;
+		bool bModeLetters = false;
+		bool bModeNumbers = false;
+		std::string strModeLeading;
+		std::string strModeMatching;
+		std::string strPublicKey;
+		bool bModeLeadingRange = false;
+		bool bModeRange = false;
+		bool bModeMirror = false;
+		bool bModeDoubles = false;
+		int rangeMin = 0;
+		int rangeMax = 0;
+		std::vector<size_t> vDeviceSkipIndex;
+		size_t worksizeLocal = 64;
+		size_t worksizeMax = 0; // Will be automatically determined later if not overriden by user
+		bool bNoCache = false;
+		size_t inverseSize = 255;
+		size_t inverseMultiple = 16384;
+		bool bMineContract = false;
+
+		argp.addSwitch('h', "help", bHelp);
+		argp.addSwitch('0', "benchmark", bModeBenchmark);
+		argp.addSwitch('1', "zeros", bModeZeros);
+		argp.addSwitch('2', "letters", bModeLetters);
+		argp.addSwitch('3', "numbers", bModeNumbers);
+		argp.addSwitch('4', "leading", strModeLeading);
+		argp.addSwitch('5', "matching", strModeMatching);
+		argp.addSwitch('6', "leading-range", bModeLeadingRange);
+		argp.addSwitch('7', "range", bModeRange);
+		argp.addSwitch('8', "mirror", bModeMirror);
+		argp.addSwitch('9', "leading-doubles", bModeDoubles);
+		argp.addSwitch('m', "min", rangeMin);
+		argp.addSwitch('M', "max", rangeMax);
+		argp.addMultiSwitch('s', "skip", vDeviceSkipIndex);
+		argp.addSwitch('w', "work", worksizeLocal);
+		argp.addSwitch('W', "work-max", worksizeMax);
+		argp.addSwitch('n', "no-cache", bNoCache);
+		argp.addSwitch('i', "inverse-size", inverseSize);
+		argp.addSwitch('I', "inverse-multiple", inverseMultiple);
+		argp.addSwitch('c', "contract", bMineContract);
+		argp.addSwitch('z', "publicKey", strPublicKey);
+		argp.addSwitch('b', "zero-bytes", bModeZeroBytes);
+
+		if (!argp.parse()) {
+			std::cout << "error: bad arguments, -h for help" << std::endl;
+			return 1;
+		}
+        if (bHelp) {
+            std::cout << g_strHelp << std::endl;
+            return 0;
+        }
+		if (strPublicKey.length() == 0) {
+			std::cout << "error: this tool requires your public key to derive it's private key security" << std::endl;
+			return 1;
+		}
 	cudaError_t error;
 	int n = 256;
 	if(argc > 1)	{ n = atoi(argv[1]);}     // Number of particles
