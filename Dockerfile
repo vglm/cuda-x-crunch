d56b2568a760e88083d7c453fa7e164976ca4617
Sieciech Czajka <sieciech@gmail.com>
2025-03-11 14:17:56 +0100

    Working on proper setting cuda architectures

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM nvidia/cuda:12.8.0-devel-ubuntu22.04 AS builder
-RUN apt update && apt install -y git cmake
+RUN apt update && apt install -y git cmake python3
 COPY . /app
 WORKDIR /app
 RUN git clean -fdx
-RUN export CUDA_ARCHITECTURES="70 75 80 86 89 90" && cmake .
+RUN export CUDA_ARCHITECTURES="61 70 75 80 86 89 90" && python3 patch_cuda_architectures.py
+RUN cmake .
 RUN make
 
 FROM ubuntu:22.04
--- patch_cuda_architectures.py
+++ patch_cuda_architectures.py
@@ -1,21 +1,16 @@
 import os
 import re
 
-# Define the path to CMakeLists.txt
 cmake_file = "CMakeLists.txt"
-
-# Get CUDA_ARCHITECTURES from environment variables
 cuda_archs = os.getenv("CUDA_ARCHITECTURES")
 
 if not cuda_archs:
     print("CUDA_ARCHITECTURES environment variable is not set.")
     exit(1)
 
-# Read the CMakeLists.txt content
 with open(cmake_file, "r") as file:
     cmake_content = file.readlines()
 
-# Replace the set(CMAKE_CUDA_ARCHITECTURES ...) line
 pattern = re.compile(r"set\(CMAKE_CUDA_ARCHITECTURES .*\)")
 new_line = f"set(CMAKE_CUDA_ARCHITECTURES {cuda_archs})\n"
 
@@ -29,11 +24,10 @@ for line in cmake_content:
         updated_content.append(line)
 
 if not found:
-    print("No matching line found in CMakeLists.txt, adding a new entry.")
-    updated_content.append(new_line)
+    raise Exception("set(CMAKE_CUDA_ARCHITECTURES ...) line not found in CMakeLists.txt")
 
 # Write the updated content back to CMakeLists.txt
 with open(cmake_file, "w") as file:
     file.writelines(updated_content)
 
-print("CMakeLists.txt has been updated successfully.")
+print("CMakeLists.txt has been updated successfully with CMAKE_CUDA_ARCHITECTURES {}".format(cuda_archs))
